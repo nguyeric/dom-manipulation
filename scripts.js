d66192f60eb0b568cab1e2082b42f6364ba13113
nguyeric <nguyeric@mail.gvsu.edu>
2019-10-22 14:10:33 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// your javascript file
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+const paragraph = document.createElement('p');
+paragraph.textContent = 'Hey I\'m red!';
+paragraph.style.color = 'red';
+
+container.appendChild(paragraph);
